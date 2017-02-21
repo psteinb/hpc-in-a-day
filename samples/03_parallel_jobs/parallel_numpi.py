b146052594eef8603b3f84e269463d5abfcc905f
Peter Steinbach <steinbac@mpi-cbg.de>
2017-02-21 15:08:22 +0100

    finished describing the shared memory parallelisation

--- samples/03_parallel_jobs/parallel_numpi.py
+++ samples/03_parallel_jobs/parallel_numpi.py
@@ -1,36 +1,47 @@
 #!/usr/bin/env python3
 import sys
-import random
+
 import numpy as np
 import math
 from multiprocessing import cpu_count, Pool
 
-def estimate_pi(total_count):
+np.random.seed(2017)
+
+def inside_circle(total_count):
 
     x = np.float32(np.random.uniform(size=total_count))
     y = np.float32(np.random.uniform(size=total_count))
 
     radii = np.sqrt(x*x + y*y)
 
-    filtered = radii[np.where(radii<1.0)]
+    count = len(radii[np.where(radii<=1.0)])
 
-    return len(filtered)
+    return count
+
+def estimate_pi(n_samples,n_cores):
+
+    partitions = [ ]
+    for i in range(n_cores):
+        partitions.append(int(n_samples/n_cores))
+
+    pool = Pool(processes=n_cores)
+    counts=pool.map(inside_circle, partitions)
+
+    total_count = sum(partitions)
+    return (4.0 * sum(counts) / total_count)
 
 if __name__=='__main__':
 
     ncores = cpu_count()
-    n_iterations = 10000
+    n_samples = 10000
     if len(sys.argv) > 1:
-        n_iterations = int(sys.argv[1])
+        n_samples = int(sys.argv[1])
 
-    partitions = [ math.ceil(n_iterations/ncores) for item in range(ncores)]
-    pool = Pool(processes=ncores)
+    partitions = [ math.ceil(n_samples/ncores) for item in range(ncores)]
 
     sizeof = np.dtype(np.float32).itemsize
-    n_iterations = sum(partitions)
-    counts=pool.map(estimate_pi, partitions)
 
-    my_pi = 4*sum(counts)/sum(partitions)
+    my_pi = estimate_pi(n_samples,ncores)
 
-    print("[parallel version] required memory %.3f MB" % (n_iterations*sizeof*3/(1024*1024)))
-    print("[using %3i cores ] pi is %f from %i samples" % (ncores,my_pi,n_iterations))
+    print("[parallel version] required memory %.3f MB" % (n_samples*sizeof*3/(1024*1024)))
+    print("[using %3i cores ] pi is %f from %i samples" % (ncores,my_pi,n_samples))
--- samples/03_parallel_jobs/serial_numpi.py
+++ samples/03_parallel_jobs/serial_numpi.py
@@ -1,31 +1,32 @@
 #!/usr/bin/env python3
 import sys
-import random
 import numpy as np
 
 np.random.seed(2017)
 
-def estimate_pi(total_count):
+def inside_circle(total_count):
 
     x = np.float32(np.random.uniform(size=total_count))
     y = np.float32(np.random.uniform(size=total_count))
 
     radii = np.sqrt(x*x + y*y)
 
-    filtered = radii[np.where(radii<1.0)]
+    count = len(radii[np.where(radii<=1.0)])
 
-    count_inside = len(filtered)
+    return count
 
-    return (4.0 * count_inside / float(total_count))
+def estimate_pi(n_samples):
+
+    return (4.0 * inside_circle(n_samples) / n_samples)
 
 if __name__=='__main__':
 
-    n_iterations = 10000
+    n_samples = 10000
     if len(sys.argv) > 1:
-        n_iterations = int(sys.argv[1])
+        n_samples = int(sys.argv[1])
 
-    my_pi = estimate_pi(n_iterations)
+    my_pi = estimate_pi(n_samples)
     sizeof = np.dtype(np.float32).itemsize
 
-    print("[serial version] required memory %.3f MB" % (n_iterations*sizeof*3/(1024*1024)))
-    print("[serial version] pi is %f from %i samples" % (my_pi,n_iterations))
+    print("[serial version] required memory %.3f MB" % (n_samples*sizeof*3/(1024*1024)))
+    print("[serial version] pi is %f from %i samples" % (my_pi,n_samples))
